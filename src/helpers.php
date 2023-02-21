a65cb84941a081420b645d010ca30a5d0c67e6d0
Clem Blanco <clement.blanco@gmail.com>
2023-02-21 08:37:47 +0100

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -8,14 +8,14 @@ if (! function_exists('configcat')) {
      * the config/features.php file. It can return a boolean or string/int based flag.
      * If no feature flag is found, false will be returned.
      *
-     * @param string $feature
+     * @param string $featureKey
      * @param mixed|null $user
      * @return bool|string|int
      */
-    function configcat(string $feature, $user = null)
+    function configcat(string $featureKey, $user = null)
     {
         return $user
-            ? ConfigCat::get($feature, $user)
-            : ConfigCat::get($feature);
+            ? ConfigCat::get($featureKey, $user)
+            : ConfigCat::get($featureKey);
     }
 }
